44136e3176802c506e2bf3f2f183c6fd2ec2b86b
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-07-19 13:08:06 +0530

    first commit

---
+++ dsa.js
@@ -0,0 +1,96 @@
+// // question-1
+
+let age = 25;
+console.log(typeof age);
+
+
+// question-2
+
+let price = 150;
+let numprice = Number(price);
+console.log(numprice); // 150
+console.log(typeof numprice);
+
+// question-3
+
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log(sum);
+let difference = num1 - num2;
+console.log(difference);
+let product = num1 * num2;
+console .log(product);
+let quotient = num1 / num2;
+console.log(quotient);
+
+
+
+// question-5
+
+let a = 20;
+let b = "5";
+let c = 2;
+let result = a + Number(b) + c;
+console.log(result);
+
+
+// question-6
+
+let score;
+console.log(score); 
+console.log(typeof score); 
+
+score = 50;
+console.log(score); 
+console.log(typeof score);
+
+// question-7
+
+var a = 10;
+a = 30;
+console.log(a);
+
+let b = 50;
+b = 60;
+console.log(b)
+
+const c = 25;
+c = 45;
+console.log(c)
+
+
+
+
+// question-8
+
+let base = 10;
+let height = "5";
+let area = 0.5 * base * Number(height);
+console.log(area); 
+
+
+// question-9
+
+let marks = "85";
+let total = Number(marks) + 15;
+console.log(total);
+
+
+// question-10
+
+let message = "Hello, World!";
+message = 42; 
+
+console.log(message);
+console.log(typeof message);
+
+
+// question-11
+
+let text = "123abc"
+let convert = Number(text);
+
+console.log(convert); 
+console.log(typeof convert);
+
