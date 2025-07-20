3cc6260331f618909912c07543f491c69db93f52
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-07-20 14:48:26 +0530

    assignment2

---
+++ assignment2.js
@@ -0,0 +1,90 @@
+// //question-1
+
+// let a = 5;
+// let b = 3;
+// let sum = a + b;
+// console.log(sum); //output: 8
+
+
+// //question-2
+
+// let x = 10;
+// x+=7; // this is equivalent to x = x+7
+// console.log(x); // output: 17
+
+
+// //question-3
+
+// let p = 4;
+// let q = 2;
+// let result = p*q;
+// console.log(result); //output:8
+
+// //question-4
+
+// let n = 20;
+// n-=5; // this is equivalent to n=n-5
+// console.log(n); //output:15
+
+//question-5
+
+
+// --> prefix increment(++i): The prefix increment the value first,and it is used in the expression.
+// --> postfix increment(i++): It is used the current value in expression,then increment.
+//example:
+//let i = 4;
+// let a = ++i; //becomes 5, then a is assigned 5
+//i=4;
+//let b = i++; //b is assigned,then i becomes 5
+// console.log(++i,a); //output:5
+// console.log(i++,b); //output:4
+//console.log(i); // output:5
+
+//question-6:
+
+//output: 5,6
+
+//explanation:
+//x = 5--> inital value of x is 5.
+//y = x++ --> postfix increment
+// y gets the current value of x,which is 5.
+//then x is incremented by 1,so x becomes 6.
+
+
+//question-7
+
+//output:6,6
+//explanation:
+//++x is the prefix increment operator.
+//first x is incremented by 1 --> x becomes 6
+//Then,the incremented value(6).
+
+//question-8
+
+//let a = 17;
+//let b = 4;
+//let sum = a/b;
+// console.log(sum); //output:1
+
+//question-9
+
+//let m = 8;
+//m/=2; // same as m = m/2
+//console.log(m) //output:4
+
+//question-10
+
+// let count = 5;
+
+// Prefix decrement
+// let result1 = --count;  
+// console.log("After prefix --count:");
+// console.log("count =", count);     // 4
+// console.log("result1 =", result1); // 4
+
+// Postfix decrement
+// let result2 = count--; 
+// console.log("After postfix count--:");
+// console.log("count =", count);     // 3
+// console.log("result2 =", result2); // 4
+
