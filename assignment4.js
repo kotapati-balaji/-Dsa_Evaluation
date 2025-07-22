469540e8c43135d7e64e466314836bc4eb68acbe
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-07-22 11:41:55 +0530

    assignment4

---
+++ assignment4.js
@@ -0,0 +1,219 @@
+//question-1
+
+//let username = "Balaji"
+//console.log(typeof username); // output: "string"
+
+//question-2
+
+//let a;
+//let b = null;
+//console.log(typeof a); // output: undefined
+//console.log(typeof b); // output: object
+
+
+//question-3
+
+//let x = 42;
+// if(typeof == "Number"){
+
+// console.log(typeof x);
+// }
+
+// question-4
+
+//let age = "18"
+//let minage = 18
+
+// console.log(age == minage);
+//console.log(age === minage);
+
+
+//question-5
+
+//let a = 10;
+//let b = 20;
+
+//if (a > b){
+// console.log(a);
+// }else{
+//     console.log(b);
+// }
+
+
+//question-6
+
+//let score = 45;
+//if score(score >=90){
+//console.log("Excellent");
+// } else if(score >= 75){
+// console.log("Good");
+//} else if (score >=50){
+// console.log("Average")
+// } else{
+// console.log("Below average")
+// }
+
+
+//question-7
+
+
+//let value = "";
+
+//if (value === ""){
+// console.log("Value is empty");
+// }
+
+
+//question-8
+
+//let age = 18;
+
+// console.log(age >= 18 ? "Eligible" : "Not Eligible");
+
+
+//question-9
+
+//let n = 17;
+
+// if (n % 2 === 0) {
+//   console.log("n is even");
+// } else {
+//   console.log("n is odd");
+// }
+
+
+//question-10
+
+//let temp;
+
+// if (typeof temp === "undefined") {
+//   console.log("Undefined");
+// } else {
+//   console.log("Defined");
+// }
+
+//question-11
+
+
+// let x = null;
+
+// if (x === null) {
+//   console.log("Null Value");
+// } else {
+//   console.log("Defined");
+// }
+
+
+//question-12
+
+// let num = -42;
+
+// if (num > 0) {
+//   console.log("Positive");
+// } else if (num < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// }
+
+
+//question-13
+
+//let month = "May";
+
+// if (month === "March") {
+//   console.log("Spring");
+// } else if (month === "April") {
+//   console.log("Spring");
+// } else if (month === "May") {
+//   console.log("Spring");
+// } else {
+//   console.log("Not Spring");
+// }
+
+
+// question-14
+
+//let temp = 25;
+
+// if (temp >= 20 && temp <= 30) {
+//   console.log("Comfortable");
+// } else {
+//   console.log("Uncomfortable");
+// }
+
+
+//question-15
+
+//let year = 2020;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//   console.log("Leap Year");
+// } else {
+//   console.log("Not a Leap Year");
+// }
+
+//question-16
+
+// Explanation:
+// 1) let val;
+// Declares a variable val without assigning a value, so its type is undefined.
+
+// 2) val = 5;
+// Assigns a number to val. Now, typeof val returns "number".
+
+// 3) val = "hello";
+// Assigns a string to val. Now, typeof val returns "string".
+
+
+//question-17
+
+//let data = 0;
+
+// console.log(data ? "Truthy" : "Falsy");
+
+
+//question-18
+
+// let person = { name: "Asha" };
+
+// console.log(typeof person);
+
+
+//question-19
+
+//let price = NaN;
+
+// if (isNaN(price)) {
+//   console.log("Not a Number");
+// } else {
+//   console.log("Valid Number");
+// }
+
+
+//question-20
+
+//let x = 15;
+
+// if (x % 3 === 0 && x % 5 === 0) {
+//   console.log("Divisible by 3 and 5");
+// } else if (x % 3 === 0) {
+//   console.log("Divisible by 3");
+// } else if (x % 5 === 0) {
+//   console.log("Divisible by 5");
+// } else {
+//   console.log("Not divisible by 3 or 5");
+// }
+
+
+//question-21
+
+// let marks = 85;
+
+// if (marks >= 75) {
+//   console.log("Distinction");
+
+//   if (marks >= 90) {
+//     console.log("Outstanding");
+//   }
+// }
