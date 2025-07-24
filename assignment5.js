c9cb0a32b46249636dd4c2e8bf5bac24f5d6c441
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-07-24 14:45:30 +0530

    assignment-5

---
+++ assignment5.js
@@ -0,0 +1,142 @@
+// question-1
+
+// let number = ("enter a number");
+
+// if (number % 2 === 0){
+//     console.log("Even");
+// } else {
+//     console.log("Odd");
+// }
+
+
+// question-2
+
+//let marks = ("Enter the student's marks (0-100)");
+//marks = number(marks);  //In this the input wil convert into number.
+
+//if (marks >= 90);{
+//console.log("Grade:A");
+// } else if (marks >=80);{
+// console.log("Grade:B");
+// } else if (marks >= 70);{
+// console.log("Grade:C");
+// } else if (marks >=60);{
+// console.log("Grade::D");
+// } else {
+    // console.log("Grade:F");
+    // }
+
+
+//question-4
+
+
+// let a = Number("Enter first number:");
+// let b = Number("Enter second number:");
+// let c = Number("Enter third number:");
+
+// let largest;
+
+// if (a >= b) {
+//   if (a >= c) {
+//     largest = a;
+//   } else {
+//     largest = c;
+//   }
+// } else {
+//   if (b >= c) {
+//     largest = b;
+//   } else {
+//     largest = c;
+//   }
+// }
+// console.log("The largest number is: " + largest);
+
+
+//question-5
+
+// let num1 = Number1;
+// let num2 = Number2;
+// let operator = prompt("Enter an operator (+, -, *, /):");
+// let result;
+
+// switch (operator) {
+//   case "+":
+//     result = num1 + num2;
+//     break;
+//   case "-":
+//     result = num1 - num2;
+//     break;
+//   case "*":
+//     result = num1 * num2;
+//     break;
+//   case "/":
+//     if (num2 !== 0) {
+//       result = num1 / num2;
+//     } 
+// }
+
+// console.log("Result: " + result);
+
+//question-6
+
+// let number = 5; //In this you can change any number
+
+// for (let i = 1; i <=10; i++){
+// console.log(`${number} x ${i} = ${number * i }`);
+// }
+
+//question-8
+
+//for (let i = 1; i <=50; i++){
+// if(i % 3 ===0 && i % 5 === 0){
+// console.log("FizzBuzz");
+// } else if (i % 3 === 0){
+// console.log("Fizz");
+// } else if(i % 5 === 0 ){
+// console.log("Buzz")
+// } else {
+    // console.log(i);
+// }
+// }
+
+//question-9
+
+// let month = ("enter the month number (1-12");
+
+// switch(month){
+// case 1: //january
+// case 3: //March
+// case 5: //May
+// case 7: //july
+// case 8: //August
+// case 10: // October
+// case 12: // December
+
+//console.log("31 days");
+//break;
+
+// case 4: //April
+// case 6: //June
+// case 9: // September
+// case 11: //November
+
+//console.log("30 days");
+//break;
+
+//case 2: // February
+//console.log("28 or 29 days");
+//break;
+
+// }
+
+//question-10
+
+// let sum = 0;
+
+// for(let i = 0; i <=100; i++){
+// if(i % 2 === 0){
+// sum+=i; // sum = sum + i
+// }
+// }
+
+// console.log(sum);
