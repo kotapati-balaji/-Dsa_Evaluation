0d9e29d9473ee09b34440efab10a1daf2b0b1104
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-07-26 13:20:57 +0530

    test

---
+++ Test.js
@@ -0,0 +1,234 @@
+// question-1
+
+//  D) both b and c
+
+
+//question-2
+
+// let greeting = "Hello";
+// greeting = 42;
+// console.log(typeof greeting);
+
+// output: "number"
+
+
+//question-3
+
+// a) for (let i = 0; i < 5; i++){
+//       console.log(i);
+// }
+
+
+// output:
+// 0
+// 1
+// 2
+// 3
+// 4
+
+
+//question-4
+
+// let x = 10;
+// if (x > 5) {
+//   x += 2;
+// } else {
+//   x -= 2;
+// }
+
+// output: x = 12
+
+
+//question-5
+
+// object is the not a primitive data type
+
+
+//question-6
+
+// for (let i = 1; i < 3; i++) {
+//   for (let j = 1; j < 3; j++) {
+//     console.log(i, j);
+//   }
+// }
+
+// output: 11,12,21,22
+
+
+//question-7
+
+// yes true 
+// It's checks both value equality and type equality
+
+// example:
+// 5 === 5 // true --> same value , same type
+// '5' === 5 // false --> same value, but different types 
+
+
+// question-8
+
+// The else if statement in JavaScript is used to  (check another condition if the first condition is false ).
+
+
+//question-9
+
+// let age = 16;
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Not an Adult");
+// }
+
+// output: not an Adult.
+
+
+//question-10
+
+// output: B) ||
+
+
+//question-11
+
+// let a = 10;
+// let b = "Balaji";
+// console.log(a,typeof a);
+// console.log(b,typeof b);
+
+//output: A) 10 Number
+//        B) Balaji string
+
+
+
+//question-12
+
+// let x = 5;
+// let y = 10;
+
+// let temp = x; // here x is store in the temp
+// x = y; // assign y value to x 
+// y = temp; // here temp is assign to y (x value is assign to y )
+
+// console.log(x);
+// console.log(y);
+
+//output: 10 , 5
+
+
+// question-13
+
+// let a = 8;
+
+// if(a % 2 === 0){
+//     console.log("Even")
+// } else{
+//     console.log("Odd")
+// }
+
+// output : Even
+
+// let a = 5;
+// if ( a % 2 === 0){
+//     console.log("Even")
+// }else{
+//     console.log("Odd")
+// }
+
+//output: Odd
+
+
+// question-14
+
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i++){
+//     sum+=i; // sum = sum + i // 0+1 = 1
+// }
+// console.log(sum);
+
+// output: 1275
+
+
+//question-15
+
+// for (i = 1; i<=10; i++){
+//     for(j=1; j<=10; j++){
+//         console.log(`${i} x ${j} = ${i*j}`)
+//     }
+// }
+
+// output:1 x 1 = 1 .....
+
+//question-16
+
+// let a = 15;
+// let b = 55;
+// let c = 27;
+
+// if(a>=b && a>=c){
+//     console.log("Largest number:", a);
+// } else if(b>=a && b>=c){
+//     console.log("Largest number:", b);
+// } else{
+//     console.log("Largest number:" , c)
+// }
+
+
+//output: Largest number: 55
+
+
+// question-17
+
+// for ( i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+// output: 10,9,8,7,6,5,4,3,2,1
+
+
+//question-18
+
+// let char = 'e';
+
+// if (char ==='a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+//     console.log("Vowel");
+// } else{
+//     console.log("Consonant")
+// }
+
+//output: Vowel
+
+
+// question-19
+
+// let num = 4;
+// num+=5;
+// console.log(num);
+
+// num-= 3;
+// console.log(num);
+
+// num*= 8;
+// console.log(num);
+
+// num /=10;
+// console.log(num);
+
+// output:
+// 9
+// 6
+// 48
+// 4.8
+
+
+//question-20
+
+// let number = -20;
+
+// if(number > 10){
+//     console.log("Positive");
+// } else if (number < 0){
+//     console.log("Negative");
+// } else{
+//     console.log("Zero")
+// }
+
+//output: Negative
