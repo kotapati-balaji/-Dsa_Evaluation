0930b7283aa316bba0b52bc87117b8ef4563259a
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-08-01 14:54:34 +0530

    assignment6

---
+++ assignment6.js
@@ -0,0 +1,105 @@
+// question-1
+
+// let num = 1;
+
+// while (num <= 10){
+//     console.log(num);
+//     num ++;
+
+// }
+
+// question-2
+
+
+// let n = parseInt(prompt("Enter a number:"));
+
+// let sum = 0;
+// let i = 1;
+// while (i <= n) {
+//   sum += i;
+//   i++;
+// }
+// console.log( n + sum);
+
+
+// question-3
+
+
+// for (let number = 1; number <= 10; number++) {
+//     let i = 1;
+//     while (i <= 10) {
+//         console.log(`${number} x ${i} = ${number * i}`);
+//         i++;
+//     }
+//     console.log('');
+// }
+
+
+// question-4
+
+// let str = "Balaji"
+
+// let reversed = "";
+// let i = str.length-1;
+
+// while (i>=0){
+//     reversed +=str[i];
+//     i--;    
+// }
+// console.log(reversed)
+
+
+//  question-5
+
+// let str = "BALAJI chowdary";
+
+// str = str.toLowerCase();
+// let a = 0;
+// let Vowelcount = 0;
+
+// while (a <= str.length){
+//     let char = str[a];
+//     if(char == 'a'|| char == 'e'|| char == 'i' || char == 'o' || char == 'u'){
+//         Vowelcount++;
+//     }
+//     a++;
+// }
+// console.log(Vowelcount)
+
+
+//  question-6
+
+// let a = 1;
+
+// while (a <= 4) {
+//   let b = 1;
+//   let stars = "";
+
+//   while (b <= a) {
+//     stars += "*";
+//     b++;
+//   }
+
+//   console.log(stars);
+//   a++;
+// }
+
+
+// question-8
+
+// let str = "helo4554heloo455154852";
+
+// let a = 0;
+// let digitcount = 0;
+
+// while ( a < str.length){
+//     let char = str[a];
+
+//     if(char > '0' && char <'9'){
+//         digitcount++;
+//     }
+//     a++;
+
+// }
+// console.log(digitcount);
+
---
+++ assignment7.js
@@ -0,0 +1,9 @@
+// question-1
+
+
+let text = "hello";
+let reversed = text.split('').reverse().join('');
+
+console.log(reversed);
+
+
