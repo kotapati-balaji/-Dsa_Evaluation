c08bea5b3011a28662ae5863c6149cc9f73b4c52
kotapati-balaji <kotapatibalajichowdary7@gmail.com>
2025-08-01 15:35:47 +0530

    assignment7

--- assignment7.js
+++ assignment7.js
@@ -1,9 +1,115 @@
 // question-1
 
 
-let text = "hello";
-let reversed = text.split('').reverse().join('');
+// let text = "hello";
+// let reversed = text.split('').reverse().join('');
 
-console.log(reversed);
+// console.log(reversed);
+
+
+// question-2
+
+// let Name = "Hello world";
+
+// Name = Name.toLowerCase();
+
+// let count = 0;
+// for(let i = 0; i < Name.length; i++){
+//     if("aeiou".includes(Name[i])){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+//  question-3
+
+// function isPalindrome(str) {
+
+//   str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//   let reversed = str.split('').reverse().join('');
+
+
+//   return str === reversed;
+// }
+// console.log(isPalindrome("madam"));  
+
+
+// question-4
+
+
+// let numbers = [5, 12, 8, 20, 3];
+// let a = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > a) {
+//     a = numbers[i];
+//   }
+// }
+
+// console.log( a);
+
+
+// question-5
+
+// let a = [10,20,30,40,50];
+// let b = 0;
+
+// for(i = 0; i< a.length; i++){
+//     b+=a[i];
+// }
+
+// console.log(b)
+
+
+// question-6
+
+// let numbers = [1, 2, 3, 2, 4, 1, 5];
+// let array = [...new Set(numbers)];
+
+// console.log(array);
+
+
+// question-7
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+
+// let twoarray = array1.concat(array2);
+
+// console.log(twoarray);
+
+
+// question-8
+
+// let array = [10, 20, 30, 40, 50];
+// let element = 30;
+
+// let index = array.indexOf(element);
+
+// console.log( index);
+
+
+// question-9
+
+// function sortAscending(arr) {
+//   return arr.sort((a, b) => a - b);
+// }
+
+// let a = [40, 10, 30, 20, 50];
+// let b = sortAscending(a);
+
+// console.log(b);
+
+
+// question-10
+
+// let a = "banana";
+// let b = "a";
+// let c = "o";
+
+// let String = a.split(b).join(c);
+
+// console.log(String);
 
 
